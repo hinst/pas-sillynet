c533955a719c2a8cfc808c23604f0b673d453af4
Savinykh Alexander <hinstance@yandex.ru>
2015-03-11 14:20:59 +0300

    preparing

---
+++ proto/echo.lpr
@@ -0,0 +1,5 @@
+program echo;
+
+begin
+end.
+
---
+++ third/synapse40/synapse40.pas
@@ -0,0 +1,20 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit synapse40;
+
+interface
+
+uses
+  blcksock, synsock, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('synapse40', @Register);
+end.
